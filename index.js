702f9b4af6408fb5c7f0a04e2d0841e43e4524ea
Daniel Pat <danielpat6@gmail.com>
2019-07-13 14:55:27 -0500

    modificando el template

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path')
 
 //Express
 const app = express();
 
 //Config
 app.set('PORT', process.env.PORT||3000);
+app.use(express.static(path.join(__dirname, '/public')))
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'pug')
 
 //Middleware
 app.use(morgan('dev'));
 
 //Routes
+app.use('/', require('./routes/home.routes'))
 
 //Server Listen
 app.listen(app.get('PORT'), _ => console.log(`server is runing ${app.get('PORT')}`))
-
