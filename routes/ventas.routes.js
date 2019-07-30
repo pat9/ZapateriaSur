93ce2a5db9c4c8f9e8787990c6351b6d3b868365
Daniel Pat <danielpat6@gmail.com>
2019-07-30 11:21:16 -0500

    Cambios

--- routes/ventas.routes.js
+++ routes/ventas.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose=require('mongoose')
 
 //Models
 const Ventas = require("../models/ventas")
@@ -18,9 +19,27 @@ router.get("/", async(req, res)=>{
            }
          }
         ])
-    console.log(ventas[0].usuario)
     res.render('Ventas/Index',{User:req.session.user, ventas})
 })
 
+router.get('/:id', async(req, res)=>{
+  console.log(req.params.id)
+  const Venta = await Ventas.aggregate([
+    {
+      '$match': {
+        '_id': mongoose.Types.ObjectId(req.params.id)
+      }
+    }, {
+      '$lookup': {
+        'from': 'users', 
+        'localField': 'idUser', 
+        'foreignField': '_id', 
+        'as': 'usuario'
+      }
+    }
+  ])
+    console.log(Venta)
+res.render('Ventas/Venta',{User:req.session.user, Venta:Venta[0],ventas:true})
+})
 
 module.exports = router;
